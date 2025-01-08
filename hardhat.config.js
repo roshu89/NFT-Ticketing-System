4c84d6dfab5ce3a299d53cddf591de436dc71f85
sarthakrana21 <sarthak.rana0321@gmail.com>
2025-01-08 14:52:25 +0530

    added volta test network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config({ path: '.env.local' });
 
+const { API_URL, PRIVATE_KEY } = process.env;
+
+
+if (!API_URL || !PRIVATE_KEY) {
+   throw new Error("Missing environment variables. Check your .env.local file.");
+}
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.11",
+   defaultNetwork: "volta",
+   networks: {
+      hardhat: {},
+      volta: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`],
+         gas: 2100000,
+         gasPrice: 800000000,
+      }
+   },
 };

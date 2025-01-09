620f6a5b1a442de255188d8ecb8397047b289bfd
sarthakrana21 <sarthak.rana0321@gmail.com>
2025-01-09 13:20:18 +0530

    fixed Big num comparisons issue

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -11,7 +11,6 @@ const EVENT_DATE = "Dec 20 2025";
 const EVENT_TIME = "12:00 PM IST";
 const EVENT_LOCATION = "Test Location";
 
-
 describe("TokenMaster", () => {
   let tokenMaster;
   let deployer, buyer;
@@ -20,98 +19,94 @@ describe("TokenMaster", () => {
     [deployer, buyer] = await ethers.getSigners();
     const TokenMaster = await ethers.getContractFactory(NAME);
     tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
-    const transaction = await tokenMaster.connect(deployer).list(EVENT_NAME, EVENT_COST, EVENT_TICKET, EVENT_DATE, EVENT_TIME, EVENT_LOCATION);
+    const transaction = await tokenMaster
+      .connect(deployer)
+      .list(EVENT_NAME, EVENT_COST, EVENT_TICKET, EVENT_DATE, EVENT_TIME, EVENT_LOCATION);
     await transaction.wait();
-  })
+  });
 
   describe("Deployment", () => {
-
     it("Sets the Name", async () => {
-      // await tokenMaster.deployed();
-      expect(await tokenMaster.name()).equal(NAME);
-      // console.log("TokenMaster address: ", tokenMaster.address);
-    })
+      expect(await tokenMaster.name()).to.equal(NAME);
+    });
 
     it("Sets the Symbol", async () => {
-      expect(await tokenMaster.symbol()).equal(SYMBOL)
-    })
+      expect(await tokenMaster.symbol()).to.equal(SYMBOL);
+    });
 
     it("Sets the Owner", async () => {
-      expect(await tokenMaster.owner()).equal(deployer.address);
-    })
-
-  })
+      expect(await tokenMaster.owner()).to.equal(deployer.address);
+    });
+  });
 
   describe("Main Events", () => {
-
     it("Updates Event count", async () => {
       const eventId = await tokenMaster.eventId();
-      expect(eventId).equal(1);
-    })
+      expect(eventId.toNumber()).to.equal(1);
+    });
+
     it("Checks Event Attribute", async () => {
       const event = await tokenMaster.getEvent(1);
-      expect(event.id).equal(1);
-      expect(event.eventName).equal(EVENT_NAME);
-      expect(event.cost).equal(EVENT_COST);
-      expect(event.maxTickets).equal(EVENT_TICKET);
-      expect(event.date).equal(EVENT_DATE);
-      expect(event.time).equal(EVENT_TIME);
-      expect(event.location).equal(EVENT_LOCATION);
-
-    })
-  })
+      expect(event.id.toNumber()).to.equal(1);
+      expect(event.eventName).to.equal(EVENT_NAME);
+      expect(event.cost.toString()).to.equal(EVENT_COST.toString());
+      expect(event.tickets).to.equal(EVENT_TICKET);
+      expect(event.date).to.equal(EVENT_DATE);
+      expect(event.time).to.equal(EVENT_TIME);
+      expect(event.location).to.equal(EVENT_LOCATION);
+    });
+  });
 
   describe("Minting", () => {
     const ID = 1;
     const SEAT = 50;
-    const AMOUNT = ethers.utils.parseUnits('1','ether');
-
+    const AMOUNT = ethers.utils.parseUnits('1', 'ether');
 
     beforeEach(async () => {
       const transaction = await tokenMaster.connect(buyer).mint(ID, SEAT, { value: AMOUNT });
       await transaction.wait();
-    })
-    
+    });
+
     it("Updates the ticket count", async () => {
       const event = await tokenMaster.getEvent(1);
-      expect(event.tickets).equal(EVENT_TICKET - 1);
-    })
+      expect(event.tickets).to.equal(EVENT_TICKET - 1);
+    });
 
     it("Updates the Buying Status", async () => {
       const hasBought = await tokenMaster.hasBought(ID, buyer.address);
-      expect(hasBought).equal(true)
-    })
+      expect(hasBought).to.equal(true);
+    });
+
     it("Updates the Seat Taken", async () => {
       const seatTaken = await tokenMaster.seatTaken(ID, SEAT);
-      expect(seatTaken).equal(buyer.address);
-    })
-    
+      expect(seatTaken).to.equal(buyer.address);
+    });
+
     it("Updates the Contract Balance", async () => {
       const balance = await ethers.provider.getBalance(tokenMaster.address);
-      expect(balance).equal(AMOUNT);
-    })
+      // eslint-disable-next-line no-unused-expressions
+      expect(balance.eq(AMOUNT)).to.be.true; // Use `.eq()` for BigNumber comparison | Using Ethers' BigNumber Methods
+    });
+  });
 
-  })
-
-  describe("Widthrawing", () => {
+  describe("Withdrawing", () => {
     const ID = 1;
     const SEAT = 50;
-    const AMOUNT = ethers.utils.parseUnits('1','ether');
-    let balanceBefore
-    
+    const AMOUNT = ethers.utils.parseUnits('1', 'ether');
+    let balanceBefore;
+
     beforeEach(async () => {
       balanceBefore = await ethers.provider.getBalance(deployer.address);
       let transaction = await tokenMaster.connect(buyer).mint(ID, SEAT, { value: AMOUNT });
       await transaction.wait();
-      transaction = await tokenMaster.connect(deployer).withdraw();;
-      await transaction.wait()
-
-    })
+      transaction = await tokenMaster.connect(deployer).withdraw();
+      await transaction.wait();
+    });
 
     it("Updates the deployer balance", async () => {
       const balanceAfter = await ethers.provider.getBalance(deployer.address);
-      expect(balanceAfter).to.greaterThan(balanceBefore);
-    })
-
-  })
-})
+      // eslint-disable-next-line no-unused-expressions
+      expect(balanceAfter.gt(balanceBefore)).to.be.true; // Use `.gt()` for BigNumber comparison | Using Ethers' BigNumber Methods
+    });
+  });
+});
